04c4a1f83c71905c74d70e92cb623ac02c278472
Robert-Andrei Damian <damian.robert.andrei@gmail.com>
2021-11-06 12:46:21 +0100

    Added timeout and animation on cursor focusing on painting

--- camera_focus.js
+++ camera_focus.js
@@ -10,14 +10,26 @@ AFRAME.registerComponent('camera_focus', {
                 return posObject.x + " " + posObject.y + " " + posObject.z;
             }
 
-            let cameraElement = document.querySelector('#rig');
+            const cursorElement = document.querySelector('#cursor');
+            const movingTimeout = setTimeout(function() {
+                let cameraElement = document.querySelector('#rig');
 
-            let camPos = objectToPos(cameraElement.getAttribute('position'))
-            console.log(camPos);
-            let targetPos = "7.18 0 1.5";
-            console.log(targetPos);
+                let camPos = objectToPos(cameraElement.getAttribute('position'))
+                let targetPos = "7.18 0 1.5";
+                cursorElement.setAttribute('animation', 'property:scale; from: 5 5 5; to: 1 1 1; dur: 0');
 
-            cameraElement.setAttribute('animation', `property:position; from: ${camPos}; to: ${targetPos}; dur:701`)
+                cameraElement.setAttribute('animation', `property:position; from: ${camPos}; to: ${targetPos}; dur:701`);
+            }, 3000)
+
+            cursorElement.setAttribute('animation', 'property: scale; from: 1 1 1; to: 5 5 5; dur: 3000')
+
+            const cameraElement = document.querySelector('#rig');
+            cameraElement.addEventListener('positionChanged',
+              function timeoutCancelerListener() {
+                  clearTimeout(movingTimeout);
+                  cameraElement.removeEventListener('positionChanged', timeoutCancelerListener);
+                  cursorElement.setAttribute('animation', 'property:scale; from: 5 5 5; to: 1 1 1; dur: 0');
+              });
         })
     },
     remove: function() {
