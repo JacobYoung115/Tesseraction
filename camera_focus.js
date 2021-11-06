d095a5387a101a01732186a5a16609261c75d687
Robert-Andrei Damian <damian.robert.andrei@gmail.com>
2021-11-06 11:40:17 +0100

    Fixed camera animation to robot picture

--- camera_focus.js
+++ camera_focus.js
@@ -6,8 +6,18 @@ AFRAME.registerComponent('camera_focus', {
                 return ;
             }
 
-            let cameraElement = evt.target.sceneEl.camera.el;
-            cameraElement.object3D.position.set(8.18, 1.47, 1.6);
+            let objectToPos = posObject => {
+                return posObject.x + " " + posObject.y + " " + posObject.z;
+            }
+
+            let cameraElement = document.querySelector('#rig');
+
+            let camPos = objectToPos(cameraElement.getAttribute('position'))
+            console.log(camPos);
+            let targetPos = "7.18 0 1.5";
+            console.log(targetPos);
+
+            cameraElement.setAttribute('animation', `property:position; from: ${camPos}; to: ${targetPos}; dur:701`)
         })
     },
     remove: function() {
