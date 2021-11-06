10936acf9b251fc0184fec28ed4ff58498447164
Robert-Andrei Damian <damian.robert.andrei@gmail.com>
2021-11-06 01:25:47 +0100

    Added raycaster-intersected listener

--- camera_focus.js
+++ camera_focus.js
@@ -1,11 +1,14 @@
 AFRAME.registerComponent('camera_focus', {
     init: function() {
-        let el = this.el;
-        console.log(this);
+        this.el.addEventListener('raycaster-intersected', function(evt) {
+            var worldPos = new THREE.Vector3();
+            worldPos.setFromMatrixPosition(evt.target.object3D.matrixWorld);
+            console.log(worldPos);
+            console.log(evt.target.object3D.position);
 
-        var pos = el.getAttribute('position');
-        console.log(pos);
-        this.el.addEventListener('click', el.getAttribute('position'));
+            let cameraPos = document.querySelector('#camera').getAttribute('position');
+            console.log(cameraPos);
+        })
     },
     remove: function() {
         this.el.removeEventListener('click', this.el.getAttribute('position'));
