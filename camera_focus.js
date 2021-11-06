15980ff762c894e3b42b606d76ab02726b9219fa
Unknown <sebacaste97@gmail.com>
2021-11-06 16:05:51 +0100

    audio

--- camera_focus.js
+++ camera_focus.js
@@ -45,3 +45,8 @@ AFRAME.registerComponent('camera_focus', {
         this.el.removeEventListener('click', this.el.getAttribute('position'));
     }
 })
+
+function play() {
+    var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+    audio.play();
+  }
